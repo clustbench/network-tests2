fed09af1676ae6111c286ef8c596589098d60521
Philip <me@crabman.me>
2016-10-28 20:36:34 +0300

    Don't fail if installation prefix already exists

--- prepare_install.sh
+++ prepare_install.sh
@@ -3,42 +3,41 @@
 INSTALL_DIR=$1
 
 
-if ! [ -d ${INSTALL_DIR} ]; 
-then        
+if ! [ -d ${INSTALL_DIR} ];
+then
 	mkdir -p ${INSTALL_DIR};
-
-	if ! [ -d ${INSTALL_DIR}/bin ];
-	then
-		mkdir ${INSTALL_DIR}/bin;         
-	fi;                        
-	if ! [ -d ${INSTALL_DIR}/etc ];
-	then       
-		mkdir ${INSTALL_DIR}/etc;
-	fi;
-    if ! [ -d ${INSTALL_DIR}/var/spool/network_tests2 ];
-	then      
-		mkdir -p ${INSTALL_DIR}/var/spool/network_tests2;
-	fi;
-	if ! [ -d ${INSTALL_DIR}/lib ];
-	then       
-        mkdir ${INSTALL_DIR}/lib;         
-    fi;                        
-    if ! [ -d ${INSTALL_DIR}/include ];
-	then   
-		mkdir ${INSTALL_DIR}/include;
-	fi;                        
-	if ! [ -d ${INSTALL_DIR}/java ];
-   	then      
-		mkdir ${INSTALL_DIR}/java;            
-	fi;                        
-	if ! [ -d ${INSTALL_DIR}/doc ]; 
-	then       
-		mkdir ${INSTALL_DIR}/doc;         
-	fi;                        
-else
-	echo "Install directory already exists";
-	exit 1;                     
 fi;
+
+if ! [ -d ${INSTALL_DIR}/bin ];
+then
+	mkdir ${INSTALL_DIR}/bin;
+fi;
+if ! [ -d ${INSTALL_DIR}/etc ];
+then
+	mkdir ${INSTALL_DIR}/etc;
+fi;
+if ! [ -d ${INSTALL_DIR}/var/spool/network_tests2 ];
+then
+	mkdir -p ${INSTALL_DIR}/var/spool/network_tests2;
+fi;
+if ! [ -d ${INSTALL_DIR}/lib ];
+then
+    mkdir ${INSTALL_DIR}/lib;
+fi;
+if ! [ -d ${INSTALL_DIR}/include ];
+then
+	mkdir ${INSTALL_DIR}/include;
+fi;
+if ! [ -d ${INSTALL_DIR}/java ];
+	then
+	mkdir ${INSTALL_DIR}/java;
+fi;
+if ! [ -d ${INSTALL_DIR}/doc ];
+then
+	mkdir ${INSTALL_DIR}/doc;
+fi;
+
+
 
 
 exit 0
