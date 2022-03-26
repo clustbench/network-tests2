cc9b120c57835a18f0c50b984c0901ced7cd7c8e
YegorCrazy <egor2002288@gmail.com>
2022-03-26 17:23:48 +0300

    minor header creation change

--- src/common/clustbench_data_write_operations.c
+++ src/common/clustbench_data_write_operations.c
@@ -28,9 +28,11 @@ int create_netcdf_header
 
 	int dims[3];
 
-	int test_type_len = strlen(test_parameters->benchmark_name);
+	int test_type_len = strlen(test_parameters->benchmark_name) + 1;
 
 	char *file_name=NULL;
+    
+    int status;
 
 	file_name=(char *)malloc(strlen(test_parameters->file_name_prefix)+strlen("_deviation.nc")+1);
 	if(file_name==NULL)
@@ -40,8 +42,10 @@ int create_netcdf_header
 
 	sprintf(file_name,"%s_%s.nc",test_parameters->file_name_prefix,file_data_type_to_string(file_data_type));
 
-	if(nc_create(file_name,NC_NOCLOBBER|NC_SHARE|NC_64BIT_OFFSET,&netcdf_file_id)!=NC_NOERR)
+    status = nc_create(file_name,NC_NOCLOBBER|NC_SHARE|NC_64BIT_OFFSET,&netcdf_file_id);
+	if(status!=NC_NOERR)
 	{
+        fprintf(stderr, "%s\n", nc_strerror(status));
 		return CREATE_FILE_ERROR;
 	}
 	free(file_name);
@@ -81,9 +85,11 @@ int create_netcdf_header
             return NETCDF_ERROR;
     }
         */
-
-	if(nc_def_var(netcdf_file_id,"test_type",NC_CHAR,1,&strings_dim_id,&test_type_var_id)!=NC_NOERR)
+        
+    status = nc_def_var(netcdf_file_id,"test_type",NC_CHAR,1,&strings_dim_id,&test_type_var_id);
+	if(status!=NC_NOERR)
     {
+        fprintf(stderr, "%s\n", nc_strerror(status));
         return NETCDF_ERROR;
     } //RENAME TO TEST_NAME
 
@@ -155,7 +161,7 @@ int create_netcdf_header
 		return NETCDF_ERROR;
 	}
 
-    int status = nc_put_var_text(netcdf_file_id,test_type_var_id,test_parameters->benchmark_name);
+    status = nc_put_var_text(netcdf_file_id,test_type_var_id,test_parameters->benchmark_name);
 	if(status!=NC_NOERR)
 	{
         fprintf(stderr, "%s\n", nc_strerror(status));
